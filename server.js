95c5b81f757760f30a7ac92044ff70707c34fd88
Sarathadevi <sarathadeviiayyappan@gmail.com>
2025-12-28 23:38:47 +0530

    backend  password update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import itemRoutes from "./routes/items.js";
 
 const app = express();
 app.use(cors({
-  origin: "*", // or your Vercel domain
+     origin: "https://taskmanager-frontend-six-theta.vercel.app",
   credentials: true
 }));
 
